97ed442f3f09d2dbcdd9704c3ffd6920ed3c32c1
Ammar Lotfy <152525723+ALotfy99@users.noreply.github.com>
2025-11-10 22:59:48 +0400

    Delete src/HeartRateInterface.java

--- src/HeartRateInterface.java
+++
@@ -1,4 +0,0 @@
-// interface for communicating heart rate information to game controller
-public interface HeartRateInterface {
-	void onHeartRate(double bpm); //to be implemented by game controller
-}
