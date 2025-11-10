5a0216c1717ca4ed7f98c4d4894fff9fd4ed5177
Ammar Lotfy <152525723+ALotfy99@users.noreply.github.com>
2025-11-10 22:52:26 +0400

    Rewrote class to be in Observer Design Pattern

--- src/ArduinoHandler.java
+++ src/ArduinoHandler.java
@@ -1,70 +1,63 @@
-// Arduino Handler is just a fancy SerialPortHandle, but with thread ability
-
+/*
+ * Ardunio Handler is a Subject Class that notifies the interested observers with the Arduino Packet recieved
+ * The interested observers can then deal with the Arduino Packet as they wish.
+ * */
+import java.util.ArrayList;
 import jssc.SerialPortException;
 
-public class ArduinoHandler extends SerialPortHandle implements Runnable {
+public class ArduinoHandler extends SerialPortHandle implements Runnable, Subject {
 	//attributes
-	int arduinoID;
-	HeartRateInterface hri;
-	PressureSensorInterface psi;
-	ButtonInterface btn;
-	static final int START_BYTE  = 0xA5;   // [AI] distinct start/handshake byte
-	static final int VIBRATION_OK = 0x88;
+	private int arduinoID;
+	private static final ArrayList<Observer> observers = new ArrayList<Observer>();
+
+	public static final int START_BYTE  = 0xA5;   // [AI] distinct start/handshake byte
+	public static final int VIBRATION_OK = 0x88;
+	private Thread t1;
+	
 	// constructor
 	public ArduinoHandler(
 			String path, 
-			int arduinoID, 
-			HeartRateInterface hri,
-			PressureSensorInterface psi,
-			ButtonInterface btn
+			int arduinoID
 			) {
 		super(path);
 		this.arduinoID = arduinoID;
-		this.hri = hri;
-		this.psi = psi;
-		this.btn = btn;
-	}
-	// decode a byte and call the necessary GameController function
-	// packet format
-	// [ARD1][ARD2][BTN1][BTN0] [PS1][PS0][PS][HR]
-	void decodeByte(byte b) throws SerialPortException {
-		boolean isCurrentArduino = ((b >>> 6) & 0x03) == (arduinoID);
-		boolean isHeartRate = (b & 0x01) == 1;
-		boolean isPressure = ((b >>> 1) & 0x01) == 1;
-		int pressureIndex = (b >>> 2) & 0x02;
-		int buttonIndex = (b >>> 4) & 0x02; //default is 11 meaning NOP;
-		
-		if (!isCurrentArduino) return; // no business for this arduino
-		System.out.println("Arduino" + arduinoID + "sent something!");
-		if (arduinoID == 1 && isHeartRate) {
-			hri.onHeartRate((double) readByte());
-		}
-		if (arduinoID == 2 && isPressure) {
-			// Pressure index comes from the header
-			psi.onPressureSensor(pressureIndex);
-		}
-		if (arduinoID == 2 && (buttonIndex != 0x03)) {
-			// Button index comes from the header
-			btn.decodeButtonOP(buttonIndex);
-		}
+		t1 = new Thread(this);
+		t1.start();
 	}
 	
 	// start function
 	@Override
 	public void run() {
-		try {
-			boolean start = (readByte() == START_BYTE); // signal start to the arduino
-			
-			while (true) { 		// wait for start signal
-				//forever run
-				if (start) { // if you start, read a control byte and then decode the control
-					byte control = readByte();
-					decodeByte(control);
-				}
+		
+		try {			
+			// thread runs
+			while (true) { 		// wait for data
+				byte control = readByte(); // program stalls to read for any byte coming
+				ArduinoPacket pkt = new ArduinoPacket(control); // establish a packet based on your data
+				notifyObservers(pkt); //notify your interested observers
 			}
 		} catch (SerialPortException e) {
 			e.printStackTrace();
 		}	
 	}
+	@Override
+	public void registerObserver(Observer o) {
+		observers.add(o);
+		
+	}
+	@Override
+	public void removeObsever(Observer o) {
+		observers.remove(o);
+		
+	}
+	@Override
+	public void notifyObservers(ArduinoPacket pkt) {
+		for (int i = 0; i < observers.size(); i++)
+		{
+			Observer observer = observers.get(i);
+			observer.update(pkt); //it is the specific observer JOB to figure out the packet mapping
+		}
+	}
+
 
 }
